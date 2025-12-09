d733b15cc77651043c3aa49f1b158e0c03aa0fba
Joe Heffernan <interim17@gmail.com>
2025-12-09 10:30:31 -0800

    remove comment

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,7 +41,6 @@ module.exports = {
             },
         },
         {
-            // keep as first gatsby-source-filesystem plugin for gatsby image support
             resolve: "gatsby-source-filesystem",
             options: {
                 path: `${__dirname}/static/pdf`,
