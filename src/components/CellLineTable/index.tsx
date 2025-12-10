9d256c1cf27e5d28c0f588e59647c4fd0fe77518
meganrm <meganrm@gmail.com>
2025-12-10 13:00:57 -0800

    remove change from other branch

--- src/components/CellLineTable/index.tsx
+++ src/components/CellLineTable/index.tsx
@@ -78,9 +78,7 @@ const CellLineTable = ({
                 // if we are not in production, make it easier to
                 // navigate to the cell line page
                 if (isClickable(record) && record.path) {
-                    navigate(record.path, {
-                        state: { fromCellCatalog: true },
-                    });
+                    navigate(record.path);
                 }
             },
         };
