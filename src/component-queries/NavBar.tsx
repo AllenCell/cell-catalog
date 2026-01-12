573c873dd046a11fbab52025d66c522a9174c137
Joe Heffernan <interim17@gmail.com>
2026-01-12 10:34:46 -0800

    use conditional rendering instead of display property for navbar menus

--- src/component-queries/NavBar.tsx
+++ src/component-queries/NavBar.tsx
@@ -1,10 +1,12 @@
 import { Flex } from "antd";
 import { graphql, useStaticQuery } from "gatsby";
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 import HamburgerMenu from "../components/HamburgerMenu/HamburgerMenu";
 import NavBarDropdown from "../components/NavBarDropdown/NavBarDropdown";
 import { formatDropdownMenuItems } from "../components/NavBarDropdown/formatDropDownMenuItems";
+import { TABLET_BREAKPOINT } from "../constants";
+import useWindowWidth from "../hooks/useWindowWidth";
 import { NavBarDropdownItem, NavBarDropdownItemGroup } from "./types";
 
 const AllenLogo = require("../img/aics-logo-white.png");
@@ -33,8 +35,6 @@ interface NavBarQueryData {
 }
 
 const NavBar: React.FC = () => {
-    const [dropdownKey, setDropdownKey] = useState(0);
-
     const data = useStaticQuery<NavBarQueryData>(graphql`
         query NavBarQuery {
             markdownRemark(frontmatter: { templateKey: { eq: "nav-bar" } }) {
@@ -78,17 +78,8 @@ const NavBar: React.FC = () => {
     // Handles edge case where menus are open during a resize that goes
     // below the mobile breakpoint by forcing remount of dropdowns when
     // crossing the breakpoint.
-    useEffect(() => {
-        const handleResize = () => {
-            const isMobile = window.innerWidth <= 768;
-            if (isMobile) {
-                setDropdownKey((prev) => prev + 1);
-            }
-        };
-
-        window.addEventListener("resize", handleResize);
-        return () => window.removeEventListener("resize", handleResize);
-    }, []);
+    const width = useWindowWidth();
+    const isTablet = width <= TABLET_BREAKPOINT;
 
     return (
         <div className={container}>
@@ -105,40 +96,48 @@ const NavBar: React.FC = () => {
                             style={{ height: "36px" }}
                         />
                     </a>
-                    <span className={divider}>|</span>
-                    <div className={dropdownButtons} key={`left-${dropdownKey}`}>
-                        <NavBarDropdown
-                            buttonComponent={
-                                <div className={titleLink}>
-                                    Allen Cell Collection
-                                </div>
-                            }
-                            label="Catalogs"
-                            items={formattedCatalogs}
-                        />
-                    </div>
+                    {!isTablet && (
+                        <>
+                        <span className={divider}>|</span>
+                        <div className={dropdownButtons}>
+                            <NavBarDropdown
+                                buttonComponent={
+                                    <div className={titleLink}>
+                                        Allen Cell Collection
+                                    </div>
+                                }
+                                label="Catalogs"
+                                items={formattedCatalogs}
+                            />
+                        </div>
+                        </>
+                    )}
                 </div>
                 <div className={rightContent}>
-                    <div className={dropdownButtons} key={dropdownKey}>
-                        <Flex gap="large">
-                            <NavBarDropdown
-                                label="Protocols"
-                                items={formattedProtocols}
-                            />
-                            <NavBarDropdown
-                                label="Collections"
-                                items={formattedDiseaseCollections}
-                            />
-                        </Flex>
-                    </div>
+                    {!isTablet && (
+                        <div className={dropdownButtons}>
+                            <Flex gap="large">
+                                <NavBarDropdown
+                                    label="Protocols"
+                                    items={formattedProtocols}
+                                />
+                                <NavBarDropdown
+                                    label="Collections"
+                                    items={formattedDiseaseCollections}
+                                />
+                            </Flex>
+                        </div>
+                    )}
                     {/* Displayed on small screens */}
-                    <div className={hamburgerMenu}>
-                        <HamburgerMenu
-                            catalogItems={formattedCatalogs}
-                            protocolItems={formattedProtocols}
-                            collectionItems={formattedDiseaseCollections}
-                        />
-                    </div>
+                    {isTablet && (
+                        <div className={hamburgerMenu}>
+                            <HamburgerMenu
+                                catalogItems={formattedCatalogs}
+                                protocolItems={formattedProtocols}
+                                collectionItems={formattedDiseaseCollections}
+                            />
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
