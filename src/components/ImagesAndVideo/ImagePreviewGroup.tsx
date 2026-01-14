c5a6d058263a66e7c923c02c8b26a3364d9d9d48
Joe Heffernan <interim17@gmail.com>
2026-01-13 16:02:44 -0800

    factor out media components for use in swipe cards on small screen

---
+++ src/components/ImagesAndVideo/ImagePreviewGroup.tsx
@@ -0,0 +1,80 @@
+import { ZoomInOutlined, ZoomOutOutlined } from "@ant-design/icons";
+import { Image, Space } from "antd";
+import { getSrc } from "gatsby-plugin-image";
+import React from "react";
+
+import { ImageOrVideo, UnpackedImageData } from "../../component-queries/types";
+
+interface ImagePreviewGroupProps {
+    mediaItems: ImageOrVideo[];
+    imageItems: UnpackedImageData[];
+    previewVisible: boolean;
+    setPreviewVisible: (visible: boolean) => void;
+    currentIndex: number;
+    setCurrentIndex: (index: number) => void;
+    setSelectedMedia: (item: ImageOrVideo) => void;
+}
+
+const {
+    previewImage,
+    toolbarWrapper,
+} = require("../../style/images-and-videos.module.css");
+
+export const PreviewGroup = ({
+    currentIndex,
+    imageItems,
+    mediaItems,
+    previewVisible,
+    setCurrentIndex,
+    setPreviewVisible,
+    setSelectedMedia,
+}: ImagePreviewGroupProps) => {
+    const allPreviewImages = imageItems.map((item, i) => {
+        return (
+            <Image
+                key={`preview-${i}`}
+                src={getSrc(item.image)}
+                style={{ display: "none" }}
+                className={previewImage}
+            />
+        );
+    });
+
+    return (
+        <Image.PreviewGroup
+            preview={{
+                visible: previewVisible,
+                className: previewImage,
+                current: currentIndex,
+                onVisibleChange: (visible) => setPreviewVisible(visible),
+                onChange: (index) => {
+                    const imageItem = imageItems[index];
+                    if (imageItem) {
+                        setSelectedMedia(imageItem);
+                        const fullIndex = mediaItems.findIndex(
+                            (item) => item === imageItem,
+                        );
+                        setCurrentIndex(fullIndex);
+                    }
+                },
+                toolbarRender: (
+                    _,
+                    { actions: { onZoomIn, onZoomOut }, transform: { scale } },
+                ) => (
+                    <Space className={toolbarWrapper}>
+                        <ZoomOutOutlined
+                            disabled={scale <= 1}
+                            onClick={onZoomOut}
+                        />
+                        <ZoomInOutlined
+                            disabled={scale >= 10}
+                            onClick={onZoomIn}
+                        />
+                    </Space>
+                ),
+            }}
+        >
+            {allPreviewImages}
+        </Image.PreviewGroup>
+    );
+};
---
+++ src/components/ImagesAndVideo/ImagesAndVideos.tsx
@@ -0,0 +1,195 @@
+import { useLocation } from "@reach/router";
+import { Flex } from "antd";
+import React, { useState } from "react";
+
+import {
+    ImageOrVideo,
+    RawVideoData,
+    UnpackedImageData,
+} from "../../component-queries/types";
+import {
+    useMobileBreakpoint,
+    useTabletBreakpoint,
+} from "../../hooks/useWidthBreakpoint";
+import { formatCellLineId } from "../../utils";
+import { isImage } from "../../utils/mediaUtils";
+import Thumbnail from "../Thumbnail";
+import { PreviewGroup } from "./ImagePreviewGroup";
+import { MediaCard } from "./MediaCard";
+
+const {
+    container,
+    header,
+    mainTitle,
+    mobileCardContainer,
+    mobileMediaCard,
+    rightTitle,
+    subtitle,
+    thumbnailContainer,
+    titleSection,
+} = require("../../style/images-and-videos.module.css");
+
+interface ImagesAndVideosProps {
+    images: UnpackedImageData[];
+    cellLineId: number;
+    videos: RawVideoData[];
+    geneSymbol: string;
+    snp?: string;
+    fluorescentTag: string;
+    parentalGeneSymbol?: string;
+    alleleTag: string;
+}
+
+const ImagesAndVideos: React.FC<ImagesAndVideosProps> = ({
+    alleleTag,
+    cellLineId,
+    fluorescentTag,
+    geneSymbol,
+    images,
+    parentalGeneSymbol,
+    videos,
+}) => {
+    const { pathname } = useLocation();
+    const isDisease = pathname.includes("disease");
+    const isTablet = useTabletBreakpoint();
+    const isMobile = useMobileBreakpoint();
+    const getVideoId = (url: string) => {
+        const match = url.match(/player\.vimeo\.com\/video\/(\d+)/);
+        return match ? match[1] : null;
+    };
+
+    const mediaItems: ImageOrVideo[] = [...images, ...videos];
+    const [selectedMedia, setSelectedMedia] = useState<ImageOrVideo | null>(
+        mediaItems[0] || null,
+    );
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [previewVisible, setPreviewVisible] = useState(false);
+
+    const showThumbnails = mediaItems.length > 1;
+
+    if (!selectedMedia) return null;
+
+    const imageItems = mediaItems.filter(isImage);
+
+    const renderThumbnail = (item: ImageOrVideo, index: number) => {
+        const isSelected = selectedMedia === item;
+
+        if (isImage(item)) {
+            return (
+                <Thumbnail
+                    key={index}
+                    image={item.image}
+                    isSelected={isSelected}
+                    onClick={() => {
+                        setSelectedMedia(item);
+                        setCurrentIndex(index);
+                    }}
+                    type="image"
+                />
+            );
+        }
+
+        const videoId = getVideoId(item.video) ?? "";
+
+        return (
+            <Thumbnail
+                key={index}
+                videoId={videoId}
+                isSelected={isSelected}
+                onClick={() => {
+                    setSelectedMedia(item);
+                    setCurrentIndex(index);
+                }}
+                type="video"
+            />
+        );
+    };
+
+    const thumbnails = showThumbnails ? (
+        <Flex vertical style={{ minHeight: 0 }} className={thumbnailContainer}>
+            {mediaItems.map(renderThumbnail).filter(Boolean)}
+        </Flex>
+    ) : undefined;
+
+    const title = (
+        <Flex
+            justify="space-between"
+            style={{ paddingTop: 24 }}
+            className={header}
+            vertical={isMobile}
+            gap={isTablet ? 8 : 0}
+        >
+            <div className={titleSection}>
+                <h3 className={mainTitle}>{formatCellLineId(cellLineId)}</h3>
+                <span className={subtitle}>
+                    {parentalGeneSymbol
+                        ? `${geneSymbol} in WTC-${fluorescentTag}-${parentalGeneSymbol} (${alleleTag}-allelic tag)`
+                        : `${geneSymbol} in WTC-${fluorescentTag} (${alleleTag}-allelic tag)`}
+                </span>
+            </div>
+            {isDisease && (
+                <span className={rightTitle}>
+                    Representative media for all clones
+                </span>
+            )}
+        </Flex>
+    );
+
+    const handleMediaClick = () => {
+        // only show preview for images, not videos
+        if (isImage(selectedMedia)) {
+            setPreviewVisible(true);
+        }
+    };
+
+    const previewGroup = imageItems.length > 0 && (
+        <PreviewGroup
+            currentIndex={currentIndex}
+            imageItems={imageItems}
+            mediaItems={mediaItems}
+            previewVisible={previewVisible}
+            setCurrentIndex={setCurrentIndex}
+            setPreviewVisible={setPreviewVisible}
+            setSelectedMedia={setSelectedMedia}
+        />
+    );
+
+    // Mobile swipable view
+    if (isTablet && mediaItems.length > 0) {
+        return (
+            <>
+                {previewGroup}
+                <div className={mobileCardContainer}>
+                    {mediaItems.map((_item, index) => (
+                        <MediaCard
+                            className={mobileMediaCard}
+                            key={index}
+                            index={index}
+                            title={title}
+                            isMobile={isMobile}
+                            item={mediaItems[index]}
+                            handleClick={handleMediaClick}
+                        />
+                    ))}
+                </div>
+            </>
+        );
+    }
+
+    return (
+        <>
+            {previewGroup}
+            <MediaCard
+                className={container}
+                title={title}
+                index={currentIndex}
+                isMobile={isMobile}
+                item={selectedMedia}
+                handleClick={handleMediaClick}
+                thumbnails={thumbnails}
+            />
+        </>
+    );
+};
+
+export default ImagesAndVideos;
